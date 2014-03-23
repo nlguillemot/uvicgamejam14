daa2d1b10b3e160523576256e4da9be78d5cbd05
Nicolas Guillemot <nlguillemot@gmail.com>
2014-03-22 22:15:27 -0700

    added perspective and view modifications for stereoscopy

--- game/main.cpp
+++ game/main.cpp
@@ -2,6 +2,7 @@
 #include <SOIL2.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <stdio.h>
 
 #include <SDL2plus.hpp>
@@ -21,12 +22,10 @@ static const char* VertexShaderSource =
         "out vec2 ftexcoord0;\n"
         "uniform mat4 modelview;\n"
         "uniform mat4 projection;\n"
-        "uniform vec4 offset;\n"
         "void main() {\n"
         "    fnormal = normal;\n"
         "    ftexcoord0 = texcoord0;\n"
-        "    vec4 pos = projection * modelview * position;\n"
-        "    gl_Position = offset * pos.w + pos;\n"
+        "    gl_Position = projection * modelview * position;\n"
         "}";
 
 static const char* FragmentShaderSource =
@@ -37,8 +36,55 @@ static const char* FragmentShaderSource =
         "uniform sampler2D diffuseTexture;\n"
         "void main() {\n"
         "    color = texture(diffuseTexture, ftexcoord0);\n"
-        "    // color = vec4((fnormal + vec4(1))/2,1);\n"
         "}";
+
+struct Scene
+{
+    GLmesh::StaticMesh mCubeMesh;
+    GLplus::Program mObjectShader;
+
+    Scene()
+    {
+        // compile shaders
+        std::shared_ptr<GLplus::Shader> vertexShader = std::make_shared<GLplus::Shader>(GL_VERTEX_SHADER);
+        vertexShader->Compile(VertexShaderSource);
+
+        std::shared_ptr<GLplus::Shader> fragmentShader = std::make_shared<GLplus::Shader>(GL_FRAGMENT_SHADER);
+        fragmentShader->Compile(FragmentShaderSource);
+
+        // attach & link
+        mObjectShader.Attach(vertexShader);
+        mObjectShader.Attach(fragmentShader);
+        mObjectShader.Link();
+
+        // load box into mesh
+        std::vector<tinyobj::shape_t> shapes;
+        tinyobj::LoadObj(shapes, "box.obj");
+        if (shapes.empty())
+        {
+            throw std::runtime_error("Expected shapes.");
+        }
+        mCubeMesh.LoadShape(shapes.front());
+    }
+
+    void Render(glm::mat4 projection, glm::mat4 viewAdjustmentForEye) const
+    {
+        glm::vec3 center(0.0f);
+        glm::vec3 eyePoint = glm::vec3(0.0f, 5.0f, 5.0f);
+        glm::vec3 up = glm::vec3(0.0f,1.0f,0.0f);
+
+        float rotation = SDL_GetTicks() / 1000.0f * 90.0f;
+        glm::mat4 modelview;
+        modelview = glm::rotate(modelview, rotation, glm::vec3(0,1,0));
+        modelview = glm::lookAt(eyePoint, center, up) * modelview;
+        modelview = viewAdjustmentForEye * modelview;
+
+        mObjectShader.UploadMatrix4("modelview", GL_FALSE, &modelview[0][0]);
+        mObjectShader.UploadMatrix4("projection", GL_FALSE, &projection[0][0]);
+
+        mCubeMesh.Render(mObjectShader);
+    }
+};
 
 void run()
 {
@@ -57,6 +103,17 @@ void run()
         ovrDevice(ovrDeviceManager->EnumerateDevices<OVR::HMDDevice>().CreateDevice(),
         [](OVR::HMDDevice* device){ device->Release(); });
 
+    if (!ovrDevice)
+    {
+        throw std::runtime_error("Couldn't find device");
+    }
+
+    OVR::HMDInfo hmdInfo;
+    if (!ovrDevice->GetDeviceInfo(&hmdInfo))
+    {
+        throw std::runtime_error("Couldn't get device info");
+    }
+
     SDL2plus::LibSDL sdl(SDL_INIT_VIDEO);
 
     sdl.SetGLAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -64,36 +121,9 @@ void run()
     sdl.SetGLAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     // create the window
-    SDL2plus::Window window(1280, 800, "Game", SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+    SDL2plus::Window window(hmdInfo.HResolution, hmdInfo.VResolution, "Game", SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
 
-    // link shaders into a program
-    GLplus::Program shaderProgram;
-    {
-        // compile shaders
-        std::shared_ptr<GLplus::Shader> vertexShader = std::make_shared<GLplus::Shader>(GL_VERTEX_SHADER);
-        vertexShader->Compile(VertexShaderSource);
-
-        std::shared_ptr<GLplus::Shader> fragmentShader = std::make_shared<GLplus::Shader>(GL_FRAGMENT_SHADER);
-        fragmentShader->Compile(FragmentShaderSource);
-
-        // attach & link
-        shaderProgram.Attach(vertexShader);
-        shaderProgram.Attach(fragmentShader);
-        shaderProgram.Link();
-    }
-
-    GLmesh::StaticMesh cubeMesh;
-    {
-        std::vector<tinyobj::shape_t> shapes;
-        tinyobj::LoadObj(shapes, "box.obj");
-        if (shapes.empty())
-        {
-            throw std::runtime_error("Expected shapes.");
-        }
-        cubeMesh.LoadShape(shapes.front());
-    }
-
-    glEnable(GL_DEPTH_TEST);
+    Scene scene;
 
     // begin main loop
     int isGameRunning = 1;
@@ -109,51 +139,29 @@ void run()
             }
         }
 
-        auto render = [&](glm::vec4 normalizedOffset, float noseToEyeDistance, float rotation) {
-            glm::vec3 center(0.0f);
-            glm::vec3 eyePoint = glm::vec3(0.0f, 5.0f, 5.0f);
-            glm::vec3 up = glm::vec3(0.0f,1.0f,0.0f);
-            glm::vec3 eyeToCenter = glm::normalize(center - eyePoint);
-            glm::vec3 across = glm::normalize(glm::cross(eyeToCenter,up));
-            eyePoint += across * noseToEyeDistance;
-
-            glm::mat4 worldview = glm::lookAt(eyePoint, center, up);
-            glm::mat4 scaleM = glm::scale(glm::mat4(), glm::vec3(1.0f,1.0f,1.0f));
-            glm::mat4 rotM = glm::rotate(glm::mat4(), rotation, glm::vec3(0.0f,1.0f,0.0f));
-            glm::mat4 modelview = rotM * scaleM;
-            modelview = worldview * modelview;
-
-            glm::mat4 projection = glm::perspective(90.0f, (float) window.GetWidth() / 2 / window.GetHeight(), 0.1f, 100.0f);
-
-            shaderProgram.UploadMatrix4("modelview", GL_FALSE, &modelview[0][0]);
-            shaderProgram.UploadMatrix4("projection", GL_FALSE, &projection[0][0]);
-            shaderProgram.UploadVec4("offset", &normalizedOffset[0]);
-
-            cubeMesh.Render(shaderProgram);
-        };
-
         glClearColor(1.0f,1.0f,1.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);;
 
-        static float noseToEyeDistance = 0.13f;
-        const float noseToEyeDistanceDelta = 0.01f;
+        OVR::Util::Render::StereoConfig stereoConfig;
+        stereoConfig.SetHMDInfo(hmdInfo);
 
-        const Uint8 *keyboardState = SDL_GetKeyboardState(NULL);
-        if (keyboardState[SDL_SCANCODE_LEFT])
-        {
-            noseToEyeDistance -= noseToEyeDistanceDelta;
-        }
-        if (keyboardState[SDL_SCANCODE_RIGHT])
-        {
-            noseToEyeDistance += noseToEyeDistanceDelta;
-        }
+        glm::mat4 lensSeparationAdjustment;
+        glm::mat4 viewAdjustmentForEye;
 
-        float rotation = SDL_GetTicks() / 1000.0f * 90.0f;
+        lensSeparationAdjustment[3][0] = stereoConfig.GetProjectionCenterOffset();
+        viewAdjustmentForEye[3][0] = stereoConfig.GetIPD();
+
         glViewport(0, 0, window.GetWidth() / 2, window.GetHeight());
-        render(glm::vec4(0.15f,0.0f,0.0f,0.0f), -noseToEyeDistance, rotation);
+        glm::mat4 leftEyeProjection = glm::perspective(stereoConfig.GetYFOVDegrees(), stereoConfig.GetAspect(), 0.1f, 100.0f);
+        scene.Render(lensSeparationAdjustment * leftEyeProjection, viewAdjustmentForEye);
+
+        lensSeparationAdjustment[3][0] *= -1;
+        viewAdjustmentForEye[3][0] *= -1;
 
         glViewport(window.GetWidth() / 2, 0, window.GetWidth() / 2, window.GetHeight());
-        render(glm::vec4(-0.15f,0.0f,0.0f,0.0f), noseToEyeDistance, rotation);
+        glm::mat4 rightEyeProjection = glm::perspective(stereoConfig.GetYFOVDegrees(), stereoConfig.GetAspect(), 0.1f, 100.0f);
+        scene.Render(lensSeparationAdjustment * rightEyeProjection, viewAdjustmentForEye);
 
         // flip the display
         window.GLSwapWindow();
